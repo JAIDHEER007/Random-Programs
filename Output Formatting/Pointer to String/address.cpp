5309ada3454834f89acebd7943caccebb96af75c
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-04-29 09:24:29 +0530

    pointer to string using function overloading
    Reference Links:
    
    https://stackoverflow.com/questions/7850125/convert-this-pointer-to-string#:~:text=%23include%20%3Csstream%3E%20//for%20std%3A%3Astringstream%20%0A%23include%20%3Cstring%3E%20%20//for%20std%3A%3Astring%0A%0Aconst%20void%20*%20address%20%3D%20static_cast%3Cconst%20void*%3E(this)%3B%0Astd%3A%3Astringstream%20ss%3B%0Ass%20%3C%3C%20address%3B%20%20%0Astd%3A%3Astring%20name%20%3D%20ss.str()%3B

---
+++ Output Formatting/Pointer to String/address.cpp
@@ -0,0 +1,68 @@
+//Printing address 
+
+#include<iostream>
+#include<sstream> //needed
+
+using namespace std;
+
+//Normally we can "print" address using cout stream but not work with it
+//For directly working on pointer with string like easiness we have to code a bit
+
+//Normal centering logic
+string center(int width,string text){
+	int len = text.length();
+	if(width < len){ return text; }
+	int diff = width - len; 
+	int padding1 = diff/2;
+	int padding2 = diff - padding1; 
+	return string(padding1,' ') + text + string(padding2,' ');
+}
+
+class node{
+	public:
+		string test = "test";
+};
+
+string inline to_string(node *address){
+	stringstream ss;
+	ss << address;		//similar to cin<<
+	return ss.str();	//returns the string from the stream
+}
+
+string inline to_string(int *address){
+	stringstream ss;
+	ss << address;
+	return ss.str();
+}
+
+string inline to_string(float *address){
+	stringstream ss;
+	ss << address;
+	return ss.str();
+}
+
+string inline to_string(string *address){
+	stringstream ss;
+	ss << address;	
+	return ss.str();
+}
+
+int main(){
+	node *test1 = new node();
+	int test2 = 553;
+	float test3 = 533.007; 
+	string test4 = "test";
+	
+	cout<<string(44,'-')<<endl;
+	cout<<"|"<<center(20,"node class")<<"||"<<center(20,to_string(test1))<<"|"<<endl;
+	cout<<string(44,'-')<<endl;
+	cout<<"|"<<center(20,"int variable")<<"||"<<center(20,to_string(&test2))<<"|"<<endl;
+	cout<<string(44,'-')<<endl;
+	cout<<"|"<<center(20,"float variable")<<"||"<<center(20,to_string(&test3))<<"|"<<endl;
+	cout<<string(44,'-')<<endl;
+	cout<<"|"<<center(20,"string variable")<<"||"<<center(20,to_string(&test4))<<"|"<<endl;
+	cout<<string(44,'-')<<endl;
+		
+	return 0;
+}
+
