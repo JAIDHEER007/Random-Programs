161526ee86d6adc1feced87b14512fbf59a41034
JAIDHEER007 <72718607+JAIDHEER007@users.noreply.github.com>
2021-06-06 22:38:19 +0530

    Keyboard Wait in C++

---
+++ Output Formatting/Keyboard_Wait.cpp
@@ -0,0 +1,14 @@
+//Keyboard Wait
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello, For every 60 seconds a minute passes in Africa!"<<endl;
+	cout<<"Together we can stop this. So, please spred the word"<<endl<<endl;
+	cout<<"Press any key to exit"<<endl;
+	
+	getchar();	//Main Focus
+	
+	return 0;
+}
